45d1e8c77bde115398cdaf9bcf248ff01b7b779b
yogeshbgit <111291109+yogeshbgit@users.noreply.github.com>
2024-01-05 17:31:19 +0530

    Update helloWord2.java

--- helloWorld/src/newHelloWorld/helloWord2.java
+++ helloWorld/src/newHelloWorld/helloWord2.java
@@ -5,6 +5,7 @@ public class helloWord2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world 2");
+		System.out.println("edited in github");
 	}
 
 }
