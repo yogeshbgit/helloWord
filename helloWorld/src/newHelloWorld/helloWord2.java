3e216874dff3d77f35ea5d48245d404020eef01f
bingivs <bingivs@gmail.com>
2024-01-05 17:29:29 +0530

    new class created

---
+++ helloWorld/src/newHelloWorld/helloWord2.java
@@ -0,0 +1,10 @@
+package newHelloWorld;
+
+public class helloWord2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world 2");
+	}
+
+}
