555fc3fc6938fc74a78e94614d62ee23b4c5beba
bingivs <bingivs@gmail.com>
2024-01-05 17:50:20 +0530

    new braanchh 1st commit

---
+++ helloWorld/src/newPackagefornewBranch/helloworldforNewBranch.java
@@ -0,0 +1,11 @@
+package newPackagefornewBranch;
+
+public class helloworldforNewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new package new class new branch");
+
+	}
+
+}
