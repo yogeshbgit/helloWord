512a8b7ad2f68df7b24c5ef428d1d00d644f1259
bingivs <bingivs@gmail.com>
2024-01-05 17:26:29 +0530

    edit from eclipse 2nd commit

--- helloWorld/src/newHelloWorld/worldHello.java
+++ helloWorld/src/newHelloWorld/worldHello.java
@@ -5,6 +5,7 @@ public class worldHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("edited in eclipse");
 	}
 
 }
